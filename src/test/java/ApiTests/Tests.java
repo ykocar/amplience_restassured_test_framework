44f4c6c7451f46e0e62f7514a9b859c055b2a4f2
nurkocar <fatmanurkocar61@gmail.comi>
2021-11-15 06:06:01 +0000

    added first Test case.

--- src/test/java/ApiTests/Tests.java
+++ src/test/java/ApiTests/Tests.java
@@ -1,4 +1,47 @@
 package ApiTests;
 
+import utilities.ConfigurationReader;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigurationReader;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Tests {
+
+    static String accessToken;
+    static String id;
+    Dotenv dotenv = Dotenv.load();
+
+    @Test
+    public void BearerToken() {
+        Response response =
+                given()
+                        .baseUri(ConfigurationReader.get("base_url"))
+                        .and().queryParam("grant_type", dotenv.get("GRANT_TYPE"))
+                        .queryParam("client_id", dotenv.get("CLIENT_ID"))
+                        .queryParam("client_secret", dotenv.get("CLIENT_SECRET"))
+                        .header("content-type", "application/x-www-form-urlencoded")
+                        .when().post("/oauth/token");
+
+        assertEquals(response.statusCode(), 200);
+
+        response.prettyPrint();
+
+        accessToken = response.path("access_token");
+
+        System.out.println(accessToken);
+
+    }
+
 }
